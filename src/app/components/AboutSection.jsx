26d3cdd39d0b07ef392753bfbd52c598ecd450cc
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-01-31 11:45:45 -0300

    concluído a pagina sobre

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,56 +1,105 @@
 "use client";
-import { React, useTransition, useState } from 'react'
-import Image from 'next/image'
-import TabButton from './TabButton'
+import { React, useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
 
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>Node.js</li>
+        <li>Postgres</li>
+        <li>Git</li>
+        <li>GitHub</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Análise e Desenvolvimento de Sistemas - Faculdade CDL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certification",
+    id: "certification",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Desenvolvimento Full Stack - Formação Dev</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
 
-    const [tab, setTab] = useState("skills")
-    const [isPending, startTransition] = useTransition()
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
 
-    const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id)
-        })
-    }
-
-    return (
-        <section className='text-white'>
-            <div className='md:grid md:grid-cols-2 gap-8 
+  return (
+    <section className="text-white">
+      <div
+        className="md:grid md:grid-cols-2 gap-8 
             flex flex-col items-center py-8 px-4 xl:gap-16 
-            sm:py-16'
+            sm:py-16"
+      >
+        <Image src="/images/sobre.png" width={400} height={400} alt="sobre" />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">Sobre mim</h2>
+          <p className="text-base lg:text-lg">
+            Sou um desenvolvedor web full stack apaixonado por criar aplicações
+            web interativas e responsivas. Tenho experiência em trabalhar com
+            JavaScript, React, Node.js, PostgreSQL, HTML, CSS e Git. Aprendo
+            rápido e estou sempre em busca de expandir meus conhecimentos e
+            habilidades. Trabalho em equipe e estou animado para trabalhar com
+            outras pessoas para criar aplicações incríveis.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              active={tab === "skills"}
+              selectTab={() => handleTabChange("skills")}
             >
-                <Image
-                    src="/images/sobre.png"
-                    width={400}
-                    height={400}
-                    alt="sobre"
-                />
-                <div>
-                    <h2 className='text-4xl font-bold text-white mb-4'>Sobre mim</h2>
-                    <p className='text-base lg:text-lg'>
-                        Sou Eduardo Carmo, desenvolvedor full stack
-                        formado em Análise e Desenvolvimento de Sistemas.
-                        Tenho paixão por transformar ideias em soluções
-                        eficientes e funcionais, sempre buscando inovação e
-                        impacto nos projetos que desenvolvemos.
-                    </p>
-                    <div className='flex flex-row mt-8'>
-                        <TabButton active={tab === "skills"} selectTab={() => handleTabChange("skills")}>
-                            Skills{" "}
-                        </TabButton>
-                        <TabButton active={tab === "education"} selectTab={() => handleTabChange("education")}>
-                            Formação{" "}
-                        </TabButton>
-                        <TabButton active={tab === "certification"} selectTab={() => handleTabChange("certification")}>
-                            Experiência{" "}
-                        </TabButton>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              active={tab === "education"}
+              selectTab={() => handleTabChange("education")}
+            >
+              {" "}
+              Formação{" "}
+            </TabButton>
+            <TabButton
+              active={tab === "certification"}
+              selectTab={() => handleTabChange("certification")}
+            >
+              {" "}
+              Certificados{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+import { FaCss3Alt, FaHtml5, FaJs, FaReact, FaNodeJs, FaBootstrap, FaGithub, FaGitAlt  } from "react-icons/fa";
+import { SiNextdotjs, SiTypescript } from "react-icons/si";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-8 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-5xl font-extrabold">
+      <div className="grid grid-cols-1 sm:grid-cols-12 ">
+        <div className="col-span-7 place-self-center text-center sm:text-left ">
+          <h1 className="text-white mb-4 text-3xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-blue-600 to-blue-400">
               Olá, Eu sou{" "}
             </span>
@@ -20,11 +22,22 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
-            Olá me chamo Eduardo Carmo, sou um desenvolvedor Full Stack com
-            conhecimento em HTML, CSS, JavaScript, React, Next.js, Node.js e
-            React Native. Eu tenho experiência em desenvolvimento web e mobile.
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-lg mb-6">
+          Sou Eduardo Carmo, desenvolvedor full stack formado em Análise e Desenvolvimento de Sistemas. 
+          Tenho paixão por transformar ideias em soluções eficientes e funcionais
           </p>
+          <div className="flex justify-center sm:justify-start gap-4 text-4xl text-[#ADB7BE] mb-6 flex-wrap">
+            <FaHtml5 className="text-orange-500" title="HTML5"/>
+            <FaCss3Alt className="text-blue-500" title="CSS3"/>
+            <FaBootstrap className="text-purple-500" title="Bootstrap"/>
+            <FaJs className="text-yellow-400" title="JavaScript"/>
+            <SiTypescript className="text-blue-400" title="TypeScript" />
+            <FaReact className="text-cyan-400" title="React"/>
+            <SiNextdotjs className="text-white" title="Next.js" />
+            <FaNodeJs className="text-green-500" title="Node.js" />
+            <FaGithub className="text-white" title="GitHub" />
+            <FaGitAlt  className="text-orange-500" title="Git" />
+          </div>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-blue-600 to-blue-800 text-white hover:opacity-80">
               Contato
@@ -36,8 +49,8 @@ const HeroSection = () => {
             </button>
           </div>
         </div>
-        <div className="col-span-4 place-self-center mt-4 lg:text-xl">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] sm:w-[250px] sm:h-[250px] lg:w-[300px] lg:h-[300px] relative">
+        <div className="col-span-5 place-self-center mt-4 lg:text-xl">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] sm:w-[250px] sm:h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/eduardo.png"
               alt="Eduardo Carmo"
