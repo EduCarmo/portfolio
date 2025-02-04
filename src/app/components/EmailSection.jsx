1a6523e024a9a4f9f5ec2e8859496a7531d52976
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-02-04 20:18:52 -0300

    melhoria na section de contato

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -48,7 +48,7 @@ const EmailSection = () => {
   return (
     <section className="grid md:grid-cols-2 my-2 md:my-12 py-24 gap-4 relative" id="contato">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-full -left-4 transform -translate-x-1/2 -translate-y-1/2 mt-12"></div>
-      <div className="z-10">
+      <div>
         <h5 className="text-xl font-bold text-white my-2">
           Vamos nos conectar
         </h5>
